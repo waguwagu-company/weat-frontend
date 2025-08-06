8317af77ae9070175cc50cdb6b64cfa5b6b11d3a
MINJEONG <lxxmnmn@gmail.com>
2025-08-06 22:41:20 +0900

    feat: 공통 칩 컴포넌트 추가

---
+++ src/components/ui/chip.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Slot } from '@radix-ui/react-slot';
+import { cva, type VariantProps } from 'class-variance-authority';
+
+import { cn } from '@/lib/utils';
+
+const chipVariants = cva(
+  'inline-flex items-center justify-center gap-2 font-semibold whitespace-nowrap border bg-background rounded-sm transition-all outline-none',
+  {
+    variants: {
+      variant: {
+        default: 'text-muted-medium border-muted-medium',
+        good: 'text-good border-good',
+        bad: 'text-bad border-bad',
+      },
+      size: {
+        default: 'w-fit h-fit px-[8px] py-[4px]',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+      size: 'default',
+    },
+  }
+);
+
+function Chip({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<'button'> &
+  VariantProps<typeof chipVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : 'button';
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(chipVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { Chip };
