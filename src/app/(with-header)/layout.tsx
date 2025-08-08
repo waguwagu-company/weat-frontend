ca23ac9e95fe37801df445dca5b95ca3fcdc128a
MINJEONG <lxxmnmn@gmail.com>
2025-08-09 01:42:10 +0900

    feat: 페이지 경로에 따라 헤더 타이틀 바인딩

--- src/app/(with-header)/layout.tsx
+++ src/app/(with-header)/layout.tsx
@@ -2,29 +2,73 @@
 
 import { ReactNode } from 'react';
 import { useRouter, usePathname } from 'next/navigation';
-import { ChevronLeft } from 'lucide-react';
+import { Share2, ChevronLeft } from 'lucide-react';
 
-export default function WithHeaderLayout({ children }: { children: ReactNode }) {
+function ShareButton() {
+  return (
+    <button
+      type="button"
+      className="absolute right-4 text-primary p-2 cursor-pointer"
+      aria-label="공유하기"
+    >
+      <Share2 width={24} height={24} />
+    </button>
+  );
+}
+
+function BackButton() {
   const router = useRouter();
-  const pathname = usePathname();
-
-  const isMap = pathname.includes('/location');
 
   return (
+    <button
+      type="button"
+      className="absolute left-4 p-2 cursor-pointer"
+      aria-label="뒤로 가기"
+      onClick={() => router.back()}
+    >
+      <ChevronLeft width={24} height={24} />
+    </button>
+  );
+}
+
+function Header() {
+  const pathname = usePathname();
+
+  const isRoot = pathname === '/';
+  const isMap = pathname.includes('/location');
+  const isLoading = pathname.includes('/loading');
+  const isResult = pathname.includes('/result');
+
+  if (isRoot || isLoading) return <></>;
+
+  const titleMap: Record<string, string> = {
+    create: '인원을 선택해 주세요.',
+    '': '내가 원하는 조건을 알려주세요.', // /[uuid]
+    location: '위치를 선택해 주세요.',
+    like: '좋아하는 음식을 알려주세요.',
+    dislike: '싫어하는 음식을 알려주세요.',
+    prompt: '중요하게 생각하는 게 뭘까요?',
+    result: '음식점을 추천 드려요!',
+  };
+
+  const segments = pathname.split('/');
+  const title =
+    segments[1] === 'create' ? titleMap['create'] : (titleMap[segments[2] || ''] ?? 'WEAT');
+
+  return (
+    <header
+      className={`sticky top-0 left-0 w-full h-13 bg-white ${isMap && 'shadow-lg'} z-1 flex items-center justify-center`}
+    >
+      {isResult ? <ShareButton /> : <BackButton />}
+      <h1 className="w-2/3 text-center font-semibold text-xl">{title}</h1>
+    </header>
+  );
+}
+
+export default function WithHeaderLayout({ children }: { children: ReactNode }) {
+  return (
     <>
-      <header
-        className={`sticky top-0 left-0 w-full h-13 bg-white ${isMap && 'shadow-lg'} z-1 flex items-center justify-center`}
-      >
-        <button
-          type="button"
-          className="absolute left-4 p-2 cursor-pointer"
-          aria-label="뒤로 가기"
-          onClick={() => router.back()}
-        >
-          <ChevronLeft className="w-6 h-6" />
-        </button>
-        <h1 className="w-2/3 text-center font-semibold text-xl">헤더</h1>
-      </header>
+      <Header />
       <main className="w-full h-[calc(100vh-52px)]">{children}</main>
     </>
   );
