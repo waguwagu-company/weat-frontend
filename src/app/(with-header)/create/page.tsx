482a659201d17dc44f491f7e7b8f919809dfd27e
MINJEONG <lxxmnmn@gmail.com>
2025-08-08 20:56:48 +0900

    style: 인원 선택 페이지 스타일링 수정

--- src/app/(with-header)/create/page.tsx
+++ src/app/(with-header)/create/page.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
+import IconGroup from '@/assets/images/icon-group.svg';
+import IconSolo from '@/assets/images/icon-solo.svg';
 
 export default function CreatePage() {
   const router = useRouter();
@@ -17,12 +19,20 @@ export default function CreatePage() {
   };
 
   return (
-    <section className="w-full h-full flex flex-col justify-center gap-[12px] p-6">
-      <Button variant="primary" className="h-1/2" onClick={startGroup}>
-        다같이
+    <section className="w-full h-full flex flex-col justify-center gap-7 p-5">
+      <Button variant="primary" className="h-1/2 flex flex-col gap-6" onClick={startGroup}>
+        <div className="flex flex-col items-center gap-3">
+          <label className="block font-cafe24-pro-up text-[32px]">WE EAT</label>
+          <IconGroup width="168" height="169" />
+        </div>
+        여러 명이 같이 갈 가게를 찾아볼래요.
       </Button>
-      <Button variant="primary" className="h-1/3" onClick={startSolo}>
-        혼자
+      <Button variant="secondary" className="h-[36%] flex flex-col gap-6" onClick={startSolo}>
+        <div className="flex flex-col items-center gap-3">
+          <label className="block font-cafe24-pro-up text-[32px]">Just mine</label>
+          <IconSolo width="100" height="101" />
+        </div>
+        혼자서 갈 가게를 찾아볼래요.
       </Button>
     </section>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 w-full text-lg font-semibold whitespace-nowrap border rounded-lg shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] transition-all disabled:pointer-events-none disabled:border-muted-light disabled:bg-background disabled:text-muted-medium [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  'inline-flex items-center justify-center gap-2 w-full text-lg font-semibold whitespace-nowrap border rounded-lg shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] transition-all disabled:pointer-events-none disabled:border-muted-light disabled:bg-background disabled:text-muted-medium shrink-0 outline-none cursor-pointer',
   {
     variants: {
       variant: {
