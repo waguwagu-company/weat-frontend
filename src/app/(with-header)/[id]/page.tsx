e5deb1de84fbe4d5018bd68e00ef09283772e880
MINJEONG <lxxmnmn@gmail.com>
2025-08-10 04:52:20 +0900

    feat: 분석 상태 조회 API 연동

--- src/app/(with-header)/[id]/page.tsx
+++ src/app/(with-header)/[id]/page.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useRouter, useParams } from 'next/navigation';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useAnalysisStatus } from '@/hooks/useAnalysis';
+import Loading from '@/components/Loading';
 import { Button } from '@/components/ui/button';
 import { RotateCw } from 'lucide-react';
 
@@ -11,39 +13,51 @@ const HEADCOUNT_MAX = 9;
 export default function MeetingPage() {
   const router = useRouter();
   const params = useParams<{ id: string }>();
-  const [headcount, setHeadcount] = useState<number>(HEADCOUNT_MIN);
+  const [href, setHref] = useState<string>('');
 
-  const getHeadcountText = (count: number) => {
+  const { data: analysisStatus, isSuccess, refetch: refetchStatus } = useAnalysisStatus(params.id);
+
+  const getCountingText = (count: number) => {
     if (count === HEADCOUNT_MIN) return '아직 아무도 조건을 알려주지 않았어요.';
     if (count === HEADCOUNT_MAX) return '모두 입력했어요.';
-    return `${headcount}명의 친구가 입력했어요.`;
+    return `${count}명의 친구가 입력했어요.`;
   };
 
-  const refreshHeadcount = () => {
-    if (headcount === HEADCOUNT_MAX) return;
-
-    setHeadcount((prev) => prev + 1);
+  const refresh = () => {
+    refetchStatus();
   };
 
-  const enterPreference = () => {
+  const copyLink = () => {
+    navigator.clipboard.writeText(href);
+  };
+
+  const enterLocation = () => {
     router.push(`/${params.id}/location`);
   };
+
+  useEffect(() => {
+    setHref(window.location.href);
+  }, []);
+
+  if (!isSuccess) return <Loading />;
+
+  const { submittedCount } = analysisStatus.data;
 
   return (
     <section className="h-full flex flex-col justify-end gap-[22vh]">
       <article>
         <h3
-          className={`font-cafe24-pro-up text-[128px] text-center ${headcount > 0 ? 'text-gradient' : 'text-muted-medium'}`}
+          className={`font-cafe24-pro-up text-[128px] text-center ${submittedCount > 0 ? 'text-gradient' : 'text-muted-medium'}`}
         >
-          {headcount}
+          {submittedCount}
         </h3>
         <div className="w-full flex justify-center items-center gap-2">
-          <p className="text-lg font-semibold">{getHeadcountText(headcount)}</p>
+          <p className="text-lg font-semibold">{getCountingText(submittedCount)}</p>
           <button
             type="button"
-            className="cursor-pointer"
+            className={`cursor-pointer active:animate-spin`}
             aria-label="새로고침"
-            onClick={refreshHeadcount}
+            onClick={refresh}
           >
             <RotateCw size={16} />
           </button>
@@ -51,10 +65,10 @@ export default function MeetingPage() {
       </article>
       <div>
         <div className="flex flex-col gap-2 p-[20px]">
-          <Button variant="secondary" className="h-fit p-[16px]">
+          <Button variant="secondary" className="h-fit p-[16px]" onClick={copyLink}>
             그룹에 초대하기
           </Button>
-          <Button variant="primary" className="h-fit p-[16px]" onClick={enterPreference}>
+          <Button variant="primary" className="h-fit p-[16px]" onClick={enterLocation}>
             나의 조건 알려주기
           </Button>
         </div>
