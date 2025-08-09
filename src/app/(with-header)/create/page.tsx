68e9558c1f94354c6a2d17bfd3c2f97b51fc6ddc
MINJEONG <lxxmnmn@gmail.com>
2025-08-10 02:21:49 +0900

    refactor: 인원 선택 로직 공통화 및 lint 에러 수정

--- src/app/(with-header)/create/page.tsx
+++ src/app/(with-header)/create/page.tsx
@@ -12,36 +12,32 @@ export default function CreatePage() {
   const { mutate, data: response, isSuccess } = useCreateGroup();
   const { isSuccess: isSuccessCategories } = useCategories();
 
-  const startGroup = () => {
-    mutate(false);
+  const start = (isSolo: boolean) => {
+    mutate(isSolo);
 
     if (isSuccess && isSuccessCategories) {
       const groupId = response.data.groupId;
-      router.push(`/${groupId}`);
-    }
-  };
-
-  const startSolo = () => {
-    mutate(true);
-
-    if (isSuccess && isSuccessCategories) {
-      const groupId = response.data.groupId;
-      router.push(`/${groupId}/location`);
+      const path = isSolo ? `/${groupId}/location` : `/${groupId}`;
+      router.push(path);
     }
   };
 
   return (
     <section className="w-full h-full flex flex-col justify-center gap-7 p-5">
-      <Button variant="primary" className="h-1/2 flex flex-col gap-6" onClick={startGroup}>
+      <Button variant="primary" className="h-1/2 flex flex-col gap-6" onClick={() => start(false)}>
         <div className="flex flex-col items-center gap-3">
-          <label className="block font-cafe24-pro-up text-[32px]">WE EAT</label>
+          <span className="block font-cafe24-pro-up text-[32px]">WE EAT</span>
           <IconGroup width="168" height="169" />
         </div>
         여러 명이 같이 갈 가게를 찾아볼래요.
       </Button>
-      <Button variant="secondary" className="h-[36%] flex flex-col gap-6" onClick={startSolo}>
+      <Button
+        variant="secondary"
+        className="h-[36%] flex flex-col gap-6"
+        onClick={() => start(true)}
+      >
         <div className="flex flex-col items-center gap-3">
-          <label className="block font-cafe24-pro-up text-[32px]">Just mine</label>
+          <span className="block font-cafe24-pro-up text-[32px]">Just mine</span>
           <IconSolo width="100" height="101" />
         </div>
         혼자서 갈 가게를 찾아볼래요.
